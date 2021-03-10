9e2c8d1eb6cc7e5660000fc1816b7c2f9c24b422
Cedric Weiss <weissc14@emea.roche.com>
2021-03-10 09:45:02 +0100

    Created ToDoContext and Startup.cs File changed

--- MVC Solution/M426_Projekt_Core/Startup.cs
+++ MVC Solution/M426_Projekt_Core/Startup.cs
@@ -2,12 +2,15 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore.SqlServer;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using M426_Projekt_Core.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace M426_Projekt_Core
 {
@@ -24,6 +27,9 @@ namespace M426_Projekt_Core
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<ToDoContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
