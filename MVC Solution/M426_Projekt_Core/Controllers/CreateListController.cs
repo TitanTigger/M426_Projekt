1728d979c017d2fd5c49549b93a2d786b9aa0d91
Ardian Demiri <ademiri@online.gibz.ch>
2021-03-10 10:13:08 +0100

    CreateListController creation
    CreateListController Instanzierung

---
+++ MVC Solution/M426_Projekt_Core/Controllers/CreateListController.cs
@@ -0,0 +1,37 @@
+﻿using M426_Projekt_Core.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace M426_Projekt_Core.Controllers
+{
+    public class CreateListController : Controller
+    {
+        private readonly ILogger<CreateListController> _logger;
+
+        public CreateListController(ILogger<CreateListController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
